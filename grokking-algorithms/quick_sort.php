656e8aa19b30878d27bf7f10fd9b3343a2bec505
Ivanov Aleksandr <33204894+Lexero@users.noreply.github.com>
2022-11-09 00:11:05 +0300

    added quick sort

---
+++ grokking-algorithms/quick_sort.php
@@ -0,0 +1,23 @@
+<?php
+
+function quickSort(array $arr)
+{
+    if (count($arr) == 1) {
+        return $arr;
+    }
+    $pivot = $arr[0];
+    $less = [];
+    $bigger = [];
+    for ($i = 1; $i < count($arr); $i++) {
+        if ($arr[$i] > $pivot) {
+            $bigger[] = $arr[$i];
+        } else {
+            $less[] = $arr[$i];
+        }
+    }
+    return array_merge(quickSort($less), [$pivot], quickSort($bigger));
+}
+
+$arr = [4, 2, 7, 1, 3];
+
+print_r(quickSort($arr));
